f4b16f11cba7d6bad84e558fd8bda665746a43a5
Patrick Murphy <murphypatv@gmail.com>
2016-06-15 16:15:41 +0100

    add jshint rules, use shelljs to run jshint and produce txt report

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #! /usr/bin/env node
-var exec = require('child_process').exec;
-var child = exec('node_modules/.bin/jshint *.js', function(err, stdout, stderr) {
-	console.log(stdout);
-});
+var shell = require('shelljs');
 
+// if reports directory doesnt exist create it
+shell.exec('mkdir -p reports');
+
+shell.exec('echo "running jshint" && node_modules/.bin/jshint index.js > reports/jshint.txt');
 
 
 
