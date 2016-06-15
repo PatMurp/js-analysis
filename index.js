eb1e62c27c748bda59ad565ae4b20f33a7ad2457
Patrick Murphy <murphypatv@gmail.com>
2016-06-15 16:26:31 +0100

    run nsp tool and create report

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 #! /usr/bin/env node
-var shell = require('shelljs');
+var shell = require('shelljs')
 
 // if reports directory doesnt exist create it
 shell.exec('mkdir -p reports');
 
+// jshint
 shell.exec('echo "running jshint" && node_modules/.bin/jshint index.js > reports/jshint.txt');
 
-
+// nsp
+shell.exec('echo "running nsp" && node_modules/.bin/nsp check > reports/nps.txt')
 
 
