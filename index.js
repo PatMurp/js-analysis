bab581ee3d12ec5f9209fc53b1ccfaab438215b9
Patrick Murphy <murphypatv@gmail.com>
2016-06-20 14:08:27 +0100

    run lint on all js file except node_modules

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #! /usr/bin/env node
+const shell = require('shelljs/global')
 const lint = require('./src/lint');
 const check = require('./src/checkModules')
 const dir = require('./src/directory')
-// if reports directory doesnt exist create it
 
-//mkdir('-p', 'reports')
+
+// if reports directory doesnt exist create it
 dir.checkDirectorySync('./reports')
 
 lint.lintFiles();
--- src/lint.js
+++ src/lint.js
@@ -1,7 +1,13 @@
 'use strict';
 const shell = require('shelljs/global');
 
+
+
 module.exports.lintFiles = function() {
+
 	return echo('running jshint') &&
-	exec('node_modules/.bin/jshint ./*.js > reports/jshint.txt');
+	exec('node_modules/.bin/jshint . > reports/jshint.txt');
 };
+
+
+
