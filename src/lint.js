4f0ea455bbd2f09cd5bc14ac1ab0c8f88b682a7b
Patrick Murphy <murphypatv@gmail.com>
2016-06-20 14:48:53 +0100

    use --exclude ./node_modules in jshint command

--- src/lint.js
+++ src/lint.js
@@ -6,7 +6,7 @@ const shell = require('shelljs/global');
 module.exports.lintFiles = function() {
 
 	return echo('running jshint') &&
-	exec('node_modules/.bin/jshint . > reports/jshint.txt');
+	exec('node_modules/.bin/jshint --exclude ./node_modules . > reports/jshint.txt');
 };
 
 
