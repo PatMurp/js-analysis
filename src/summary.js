b2388ecf5392565efc8d413a969f35bde0bd46c6
Patrick Murphy <murphypatv@gmail.com>
2016-06-21 17:55:56 +0100

    refactor summary file creation

--- src/summary.js
+++ src/summary.js
@@ -8,12 +8,24 @@ const summary = 'reports/summary.txt';
 // build report summary
 module.exports.fileTail = function() {
 	echo(heading).toEnd(summary);
+	jshintSummary();
+	eslintSummary();
+	jsinspectSummary();
+};
+
+function jshintSummary() {
 	echo('jshint ').toEnd(summary) &&
 	exec('tail -1 reports/jshint.txt | head -1')
 	.toEnd(summary + '\n');
+};
+
+function eslintSummary() {
 	echo('eslint ').toEnd(summary) &&
 	exec('tail -2 reports/eslint.txt | head -1')
 	.toEnd(summary + '\n');
+};
+
+function jsinspectSummary() {
 	echo('jsinspect ').toEnd(summary) &&
 	exec('tail -1 reports/jsinspect.txt | head -1')
 	.toEnd(summary + '\n');
