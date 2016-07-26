66e564fb4ab9d02df73be1f27964b132d3afb3f6
Patrick Murphy <murphypatv@gmail.com>
2016-07-26 15:22:41 +0100

    move lint rules & config files to linters folder

--- src/copy.js
+++ src/copy.js
@@ -4,28 +4,26 @@ const fs = require('fs');
 module.exports.copyConfig = function() {
 	jshintRules();
 	jshintIgnore();
-	eslintRules();
-	eslintIgnore();
 };
 
 function jshintRules() {
-	fs.createReadStream('node_modules/js-analysis/.jshintrc').
+	fs.createReadStream('node_modules/js-analysis/linters/.jshintrc').
 	pipe(fs.createWriteStream('.jshintrc'));
 };
 
 function jshintIgnore() {
-	fs.createReadStream('node_modules/js-analysis/.jshintignore').
+	fs.createReadStream('node_modules/js-analysis/linters/.jshintignore').
 	pipe(fs.createWriteStream('.jshintignore'));
 }
 
 
 function eslintRules() {
-	fs.createReadStream('node_modules/js-analysis/.eslintrc.js').
+	fs.createReadStream('node_modules/js-analysis/linters/.eslintrc.js').
 	pipe(fs.createWriteStream('.eslintrc.js'));
 };
 
 function eslintIgnore() {
-	fs.createReadStream('node_modules/js-analysis/.eslintignore').
+	fs.createReadStream('node_modules/js-analysis/linters/.eslintignore').
 	pipe(fs.createWriteStream('.eslintignore'));
 };
 
